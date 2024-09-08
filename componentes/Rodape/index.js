2f6140354c009e843dab4d9e8a93c32cfcb8fa60
Lucas Barboza <105080711+LucasuKo@users.noreply.github.com>
2024-09-08 11:30:55 -0300

    Projeto-organo

---
+++ componentes/Rodape/index.js
@@ -0,0 +1,13 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (<footer className="footer">
+        <section>
+            <p>
+                Desenvolvido por Lucas Barboza.
+            </p>
+        </section>
+    </footer>)
+}
+
+export default Rodape
